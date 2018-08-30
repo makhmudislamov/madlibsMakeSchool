d974cedb024dda0b471c0d528b852096958dbc13
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-08-29 17:42:21 -0700

    initial MVP

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,8 @@
 print("Welcome to Madlibs.")
 name = input("What is your name? ")
 print (name)
+adjective1 = input("Give me first adjective: ")
+name2 = input("Give me name: ")
+animal = input("give me animal: ")
+
+print ("some text " + name + "some text ")
